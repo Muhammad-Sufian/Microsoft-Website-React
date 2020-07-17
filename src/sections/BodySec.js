5bce04ad236f291753316fd8ba89c7c27c6bd594
Muhammad Sufian <67870526+Muhammad-Sufian@users.noreply.github.com>
2020-07-17 23:52:31 +0500

    React-Tutorial-MicrosoftWebpage

---
+++ src/sections/BodySec.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import "../App.css";
+
+class BodySec extends React.Component {
+  render(){
+    return (
+      <div className="BodySec">
+        {/* background: url('https://i.ibb.co/zGSDGCL/slide1.png') no-repeat center center/cover; */ }
+        <section className="Section1">
+            <div className="Sec1-Div1">
+                <div className="Sec1-Div1-childnode">
+                    <div className="Sec1-Div1-text">
+                        <h3>Surface Deals</h3>
+                        <p>Select Surfaces are on sale now - save while supplies last</p>
+                    </div>
+                    <div className="Sec1-Div1-bttn"><button id="Sec1Bttn">Shop Now</button></div>
+                </div>
+            </div>
+            <div className="Sec1-Div2">
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"> <img src="https://i.ibb.co/LZPVKq9/card1.png" style={{width:"100%"}}/> </div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>New Surface Pro 7</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>See how Katie Sowers, Asst. Coach for the 49ers, uses Surface Pro 7 to put her plans into play.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Learn More</b></a></div>
+                </div>
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"><img src="https://i.ibb.co/KjGFHVJ/card2.png" style={{width:"100%"}}/></div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>New Surface Laptop 3</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>Express yourself powerfully with a thin, light, and elegant design,faster performance, and up to 11.5 hours battery life.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Learn More</b></a></div>
+                </div>
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"> <img src="https://i.ibb.co/2cnshH6/card3.png" style={{width:"100%"}}/></div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>Save $150 + free controller</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>Buy an Xbox One X console and double your fun with a free select extra controller. Starting at $349.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Learn More</b></a></div>
+                </div>
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"> <img src="https://i.ibb.co/G57P0Pb/card4.png" style={{width:"100%"}}/></div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>The new Microsoft Edge</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>Expect more. World class performance, with more privacy, more productivity, and more value.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Learn More</b></a></div>
+                </div>
+            </div>
+        </section>
+
+        <section className="Section2">
+        <div className="Sec2-Div1">
+                <div className="Sec2-Div1-childnode">
+                    <div className="Sec2-Div1-text">
+                        <h3>Xbox Game Pass Ultimate</h3>
+                        <p>Xbox Game Pass Ultimate Xbox Live Gold and over 100 high-quality console and PC games. Play together with friends and discover your next favorite game</p>
+                    </div>
+                    <div className="Sec2-Div1-bttn"><button id="Sec2Bttn"><b>Join Now</b></button></div>
+                </div>
+            </div>
+            <div className="Sec1-Div2">
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec2-Div2-Subdiv1-pic"> <img src="https://i.ibb.co/zVqhWn2/card5.png" style={{width:"100%"}}/> </div>
+                    <div className="Sec2-Div2-Subdiv1-heading"><h3>Microsoft Teams</h3></div>
+                    <div className="Sec2-Div2-Subdiv1-text"><p>Unleash the power of your team.</p></div>
+                    <div className="Sec2-Div2-Subdiv1-link"><a href="#"><b>Shop Now</b></a></div>
+                </div>
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"><img src="https://i.ibb.co/mGZcxcn/card6.jpg" style={{width:"100%"}}/></div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>Unlock the power of learning</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>Get students future-ready with Windows 10 devices. Starting at $219.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Shop Now</b></a></div>
+                </div>
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"> <img src="https://i.ibb.co/NpPvVHj/card7.png" style={{width:"100%"}}/></div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>Windows 10 Enterprise</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>Download the free 90-day evaluation for IT professionals.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Download Now </b></a></div>
+                </div>
+                <div className="Sec1-Div2-Subdiv1">
+                    <div className="Sec1-Div2-Subdiv1-pic"> <img src="https://i.ibb.co/LkP4L5T/card8.png" style={{width:"100%"}}/></div>
+                    <div className="Sec1-Div2-Subdiv1-heading"><h3>Explore Kubernetes</h3></div>
+                    <div className="Sec1-Div2-Subdiv1-text"><p>Learn how Kubernetes works and get started with cloud native app development today.</p></div>
+                    <div className="Sec1-Div2-Subdiv1-link"><a href="#"><b>Get Started</b></a></div>
+                </div>
+            </div>
+        </section>
+
+        <section className="Section3">
+            <div className="Sec3-Div1">
+                <div className="Sec3-Div1-childnode">
+                        <div className="Sec3-Div1-text">
+                            <h3>Commiting To Carbon Negative</h3>
+                            <p>Microsoft will be carbon negative by 2030 and by 2050 we will remove all carbon the company has emitted since it was founded in 1975</p>
+                        </div>
+                    <div className="Sec3-Div1-bttn"><button id="Sec3Bttn"><b>Learn More</b></button></div>
+                </div>
+            </div>
+            <div className="Sec3-Div2">
+                <div style={{marginLeft: "10px", marginRight: "10px"}}>Follow Microsoft</div>
+                <div style={{marginLeft: "10px", marginRight: "10px"}}><img src="https://i.ibb.co/LrVMXNR/social-fb.png"/></div>
+                <div style={{marginLeft: "10px", marginRight: "10px"}}><img src="https://i.ibb.co/vJvbLwm/social-twitter.png"/></div>
+                <div style={{marginLeft: "10px", marginRight: "10px"}}><img src="https://i.ibb.co/b30HMhR/social-linkedin.png"/></div>
+            
+            </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default BodySec;
